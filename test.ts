c7759a6c975015ac3ddefba8d9b849a9ab67d2be
yanndebelgique <yann.buydens@gmail.com>
2022-09-12 13:44:53 +0200

    fix case 4.1

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { analyse_and_report } from "./src/analyse_directory/analyse_and_report";
 
 analyse_and_report({
-  dirPath: './test_directories/case_3_2'
+  dirPath: './test_directories/case_3_3'
 })
